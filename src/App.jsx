7d1ed8da7def05af38671cd6a4d5a1395c9bf058
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-14 09:35:10 +0200

    Import and add Meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Header />
+     <Meme />
     </>
   )
 }
